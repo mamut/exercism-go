e8e1972d35bfe8b3f243d196d473dd670f2f6e5d
Krzysztof Urban <krzysztof.urban@netguru.pl>
2023-10-16 15:55:47 +0200

    solve blackjack

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -2,11 +2,62 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten":
+		return 10
+	case "jack":
+		return 10
+	case "queen":
+		return 10
+	case "king":
+		return 10
+	default:
+		return 0
+	}
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	panic("Please implement the FirstTurn function")
+	cardSum := ParseCard(card1) + ParseCard(card2)
+	dealerValue := ParseCard(dealerCard)
+
+	switch {
+	case cardSum == 22:
+		return "P"
+	case cardSum == 21:
+		if dealerValue < 10 {
+			return "W"
+		} else {
+			return "S"
+		}
+	case cardSum >= 17:
+		return "S"
+	case cardSum >= 12:
+		if dealerValue < 7 {
+			return "S"
+		} else {
+			return "H"
+		}
+	default:
+		return "H"
+	}
 }
