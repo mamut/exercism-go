7031c378d5a7c21941ef89b5f15e4c7d9a65f5b7
Krzysztof Urban <krzysztof.urban@netguru.pl>
2023-10-16 14:00:58 +0200

    solve weather-forecast

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides current weather forecast.
 package weather
 
+// CurrentCondition is the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation is the current weather location.
 var CurrentLocation string
 
+// Forecast returns today's forecast based on the current weather condition and location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
