035a564cb3501e7ad42166d946305da4d0e8468a
Krzysztof Urban <krzysztof.urban@netguru.pl>
2023-10-16 15:05:42 +0200

    solve vehicle-purchase

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -2,15 +2,29 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	return kind == "car" || kind == "truck"
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	var choice string
+
+	if option1 < option2 {
+		choice = option1
+	} else {
+		choice = option2
+	}
+
+	return choice + " is clearly the better choice."
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age >= 10.0 {
+		return originalPrice * 0.5
+	} else if age >= 3.0 {
+		return originalPrice * 0.7
+	} else {
+		return originalPrice * 0.8
+	}
 }
