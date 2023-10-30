9a19b31731eefa1d719413eca6da613432f82b3c
Krzysztof Urban <krzysztof.urban@netguru.pl>
2023-10-30 22:50:05 +0100

    solve logs-logs-logs

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,44 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, ch := range log {
+		if ch == '❗' {
+			return "recommendation"
+		}
+
+		if ch == '🔍' {
+			return "search"
+		}
+
+		if ch == '☀' {
+			return "weather"
+		}
+	}
+
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	newLog := ""
+
+	for _, ch := range log {
+		if ch == oldRune {
+			newLog += string(newRune)
+		} else {
+			newLog += string(ch)
+		}
+	}
+
+	return newLog
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCountInString(log) <= limit
 }
