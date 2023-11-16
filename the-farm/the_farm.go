01381ea3a116402cbf71c451909e1fbb2443f26e
Krzysztof Urban <krzysztof.urban@netguru.pl>
2023-11-16 14:30:57 +0100

    solve the-farm

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -1,16 +1,51 @@
 package thefarm
 
-// TODO: define the 'DivideFood' function
+import (
+	"errors"
+	"fmt"
+)
 
-// TODO: define the 'ValidateInputAndDivideFood' function
+func DivideFood(fodderCalculator FodderCalculator, nrOfCows int) (float64, error) {
+	fodderAmount, err := fodderCalculator.FodderAmount(nrOfCows)
 
-// TODO: define the 'ValidateNumberOfCows' function
+	if err != nil {
+		return 0, err
+	}
 
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
+	fatteningFactor, err := fodderCalculator.FatteningFactor()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return fodderAmount * fatteningFactor / float64(nrOfCows), nil
+}
+
+func ValidateInputAndDivideFood(fodderCalculator FodderCalculator, nrOfCows int) (float64, error) {
+	if nrOfCows <= 0 {
+		return 0, errors.New("invalid number of cows")
+	}
+
+	return DivideFood(fodderCalculator, nrOfCows)
+}
+
+type InvalidCowsError struct {
+	nrOfCows int
+	message  string
+}
+
+func (e InvalidCowsError) Error() string {
+	return fmt.Sprintf("%d cows are invalid: %s", e.nrOfCows, e.message)
+}
+
+func ValidateNumberOfCows(nrOfCows int) error {
+	if nrOfCows < 0 {
+		return &InvalidCowsError{nrOfCows, "there are no negative cows"}
+	}
+
+	if nrOfCows == 0 {
+		return &InvalidCowsError{nrOfCows, "no cows don't need food"}
+	}
+
+	return nil
+}
