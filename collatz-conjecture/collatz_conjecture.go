7125c9afc2bb23d5386ff67fb929f9c8b9cad809
Krzysztof Urban <krzysztof.urban@netguru.pl>
2023-11-16 16:10:55 +0100

    solve collatz-conjecture

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,25 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, errors.New("n must be positive")
+	}
+
+	for i := 0; ; i++ {
+		if n == 1 {
+			return i, nil
+		}
+
+		n = CollatzStep(n)
+	}
+}
+
+func CollatzStep(n int) int {
+	if n%2 == 0 {
+		return n / 2
+	} else {
+		return 3*n + 1
+	}
 }
