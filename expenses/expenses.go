2589a9833d242b23ba4044af5cb8953779143f25
Krzysztof Urban <krzysztof.urban@netguru.pl>
2023-11-16 15:42:24 +0100

    solve expenses

--- expenses/expenses.go
+++ expenses/expenses.go
@@ -1,5 +1,10 @@
 package expenses
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Record represents an expense record.
 type Record struct {
 	Day      int
@@ -15,26 +20,45 @@ type DaysPeriod struct {
 
 // Filter returns the records for which the predicate function returns true.
 func Filter(in []Record, predicate func(Record) bool) []Record {
-	panic("Please implement the Filter function")
+	out := []Record{}
+
+	for _, record := range in {
+		if predicate(record) {
+			out = append(out, record)
+		}
+	}
+
+	return out
 }
 
 // ByDaysPeriod returns predicate function that returns true when
 // the day of the record is inside the period of day and false otherwise.
 func ByDaysPeriod(p DaysPeriod) func(Record) bool {
-	panic("Please implement the ByDaysPeriod function")
+	return func(record Record) bool {
+		return p.From <= record.Day && record.Day <= p.To
+	}
 }
 
 // ByCategory returns predicate function that returns true when
 // the category of the record is the same as the provided category
 // and false otherwise.
 func ByCategory(c string) func(Record) bool {
-	panic("Please implement the ByCategory function")
+	return func(record Record) bool {
+		return c == record.Category
+	}
 }
 
 // TotalByPeriod returns total amount of expenses for records
 // inside the period p.
 func TotalByPeriod(in []Record, p DaysPeriod) float64 {
-	panic("Please implement the TotalByPeriod function")
+	filtered := Filter(in, ByDaysPeriod(p))
+
+	total := 0.0
+	for _, record := range filtered {
+		total += record.Amount
+	}
+
+	return total
 }
 
 // CategoryExpenses returns total amount of expenses for records
@@ -42,5 +66,11 @@ func TotalByPeriod(in []Record, p DaysPeriod) float64 {
 // An error must be returned only if there are no records in the list that belong
 // to the given category, regardless of period of time.
 func CategoryExpenses(in []Record, p DaysPeriod, c string) (float64, error) {
-	panic("Please implement the CategoryExpenses function")
+	filtered := Filter(in, ByCategory(c))
+
+	if len(filtered) == 0 {
+		return 0.0, errors.New(fmt.Sprintf("unknown category %s", c))
+	}
+
+	return TotalByPeriod(filtered, p), nil
 }
