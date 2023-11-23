8592460587bf710570b10bb017db7dfac7837882
Krzysztof Urban <krzysztof.urban@netguru.pl>
2023-11-23 17:03:40 +0100

    solve luhn

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,5 +1,58 @@
 package luhn
 
+import (
+	"strconv"
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	digits, err := toDigits(id)
+
+	if len(digits) <= 1 {
+		return false
+	}
+
+	if err != nil {
+		return false
+	}
+
+	for i := 0; i < len(digits); i++ {
+		if i%2 == 0 {
+			continue
+		}
+
+		offset := len(digits) - i - 1
+
+		newDigit := digits[offset] * 2
+		if newDigit > 9 {
+			newDigit -= 9
+		}
+
+		digits[offset] = newDigit
+	}
+
+	sum := 0
+	for _, digit := range digits {
+		sum += digit
+	}
+
+	return sum%10 == 0
+}
+
+func toDigits(id string) ([]int, error) {
+	digits := []int{}
+
+	for _, ch := range id {
+		if ch == ' ' {
+			continue
+		}
+
+		digit, err := strconv.Atoi(string(ch))
+		if err != nil {
+			return nil, err
+		}
+
+		digits = append(digits, digit)
+	}
+
+	return digits, nil
 }
